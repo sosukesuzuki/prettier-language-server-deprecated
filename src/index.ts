df18f01bfcf217ad95ff6743d0621475159441a7
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-05-06 17:53:02 +0900

    Implement minimal formatting server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,53 @@
+import { TextDocument } from "vscode-languageserver-textdocument";
+import {
+  InitializeResult,
+  ProposedFeatures,
+  Range,
+  TextDocumentSyncKind,
+  TextDocuments,
+  TextEdit,
+  createConnection,
+} from "vscode-languageserver/node.js";
+
 export function createPrettierLanguageServer() {
-  // do nothing
+  const connection = createConnection(ProposedFeatures.all);
+
+  const documents: TextDocuments<TextDocument> = new TextDocuments(
+    TextDocument
+  );
+
+  connection.onInitialize(() => {
+    const result: InitializeResult = {
+      capabilities: {
+        textDocumentSync: TextDocumentSyncKind.Incremental,
+        documentFormattingProvider: true,
+        documentRangeFormattingProvider: true,
+      },
+    };
+    return result;
+  });
+
+  connection.onDocumentFormatting(async (params) => {
+    const document = documents.get(params.textDocument.uri);
+    if (document === undefined) {
+      return [];
+    }
+    const text = document.getText();
+    const pos0 = document.positionAt(0);
+    const pos1 = document.positionAt(text.length);
+    return [TextEdit.replace(Range.create(pos0, pos1), text.toUpperCase())];
+  });
+
+  connection.onDocumentRangeFormatting(async (params) => {
+    const document = documents.get(params.textDocument.uri);
+    if (document === undefined) {
+      return [];
+    }
+    const text = document.getText(params.range);
+    return [TextEdit.replace(params.range, text.toUpperCase())];
+  });
+
+  documents.listen(connection);
+
+  connection.listen();
 }
