b4fa5ba5d8d0a9d4f341ff7748a7b82527217934
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-05-13 18:42:40 +0900

    Implement ConsoleLoggingService

--- src/LoggingService.ts
+++ src/LoggingService.ts
@@ -1,6 +1,43 @@
+import { Connection } from "vscode-languageserver";
+
 export interface LoggingService {
   logDebug(message: string, data?: unknown): void;
   logInfo(message: string, data?: unknown): void;
   logWarning(message: string, data?: unknown): void;
   logError(message: string, data?: unknown): void;
+}
+
+export class ConsoleLoggingService implements LoggingService {
+  constructor(private connection: Connection) {}
+
+  logDebug(message: string, data?: unknown): void {
+    this.connection.console.log(message);
+    if (data != null) {
+      this.logObj(data, "log");
+    }
+  }
+
+  logInfo(message: string, data?: unknown): void {
+    this.connection.console.info(message);
+    if (data != null) {
+      this.logObj(data, "info");
+    }
+  }
+
+  logWarning(message: string, data?: unknown): void {
+    this.connection.console.warn(message);
+    if (data != null) {
+      this.logObj(data, "warn");
+    }
+  }
+  logError(message: string, data?: unknown): void {
+    this.connection.console.error(message);
+    if (data != null) {
+      this.logObj(data, "error");
+    }
+  }
+
+  private logObj(data: unknown, loglevel: "log" | "info" | "warn" | "error") {
+    this.connection.console[loglevel](JSON.stringify(data, null, 2));
+  }
 }
