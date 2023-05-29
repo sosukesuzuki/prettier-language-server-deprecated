6b4f006d1a96982fec6e7a4796e0d4aa88cf01cd
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-05-29 16:21:48 +0900

    Implement `provideEdits`

--- src/PrettierEditService.ts
+++ src/PrettierEditService.ts
@@ -11,6 +11,7 @@ import {
 import { ConnectionService } from "./ConnectionService";
 import { URI } from "vscode-uri";
 import { getParserFromLanguageId } from "./languageFilter";
+import { Range, TextEdit } from "vscode-languageserver";
 
 export class PrettierEditService {
   constructor(
@@ -18,6 +19,49 @@ export class PrettierEditService {
     private loggingService: LoggingService,
     private moduleResolver: ModuleResolverInterface
   ) {}
+
+  private provideEdits = async (
+    document: TextDocument,
+    options: ExtensionFormattingOptions
+  ): Promise<TextEdit[]> => {
+    const startTime = new Date().getTime();
+    const result = await this.format(document.getText(), document, options);
+    if (!result) {
+      // No edits happened, return never so VS Code can try other formatters
+      return [];
+    }
+    const duration = new Date().getTime() - startTime;
+    this.loggingService.logInfo(`Formatting completed in ${duration}ms.`);
+    const edit = this.minimalEdit(document, result);
+    return [edit];
+  };
+
+  private minimalEdit(document: TextDocument, string1: string) {
+    const string0 = document.getText();
+    // length of common prefix
+    let i = 0;
+    while (
+      i < string0.length &&
+      i < string1.length &&
+      string0[i] === string1[i]
+    ) {
+      ++i;
+    }
+    // length of common suffix
+    let j = 0;
+    while (
+      i + j < string0.length &&
+      i + j < string1.length &&
+      string0[string0.length - j - 1] === string1[string1.length - j - 1]
+    ) {
+      ++j;
+    }
+    const newText = string1.substring(i, string1.length - j);
+    const pos0 = document.positionAt(i);
+    const pos1 = document.positionAt(string0.length - j);
+
+    return TextEdit.replace(Range.create(pos0, pos1), newText);
+  }
 
   private async format(
     text: string,
